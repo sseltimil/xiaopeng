abc47e501c67c19225d2d3ece6d4a7431cff0eba
Yinghan Wang <wangx6@gmail.com>
2025-10-21 14:56:38 +0100

    asdqdf

--- app.js
+++ app.js
@@ -5,7 +5,24 @@
   list component
 */
 
-class BaseViewComponent {
+
+class Emitter{
+  on = {};
+  reg(event, fn) {
+    if(!this.on[event]) {
+      this.on[event] = [];
+    }
+    this.on[event].push(fn);
+  }
+
+  emit(event, data) {
+    if(this.on[event]) {
+      this.on[event].forEach(fn => fn(data));
+    }
+  }
+}
+
+class BaseViewComponent{
   rootEle = null;
   constructor(option) {
     
@@ -14,7 +31,6 @@ class BaseViewComponent {
       console.log(this.rootEle, '--------');
       this.rootEle.className = option.class;
     }
-    
   }
 }
 class App extends BaseViewComponent {
@@ -31,12 +47,21 @@ class App extends BaseViewComponent {
     this.TodoService = new TodoService();
 
     this.initApp();
+    this.initEvent();
   }
 
   initApp() {
     this.rootEle.appendChild(this.title.rootEle);
+
+    console.log(this.todoFilter);
     this.rootEle.appendChild(this.todoFilter.rootEle);
     this.rootEle.appendChild(this.todoList.rootEle);
+  }
+
+  initEvent() {
+    this.todoFilter.output.reg('onclickfilter', (data) => {
+      console.log(data);
+    });
   }
 }
 class Title extends BaseViewComponent {
@@ -45,10 +70,52 @@ class Title extends BaseViewComponent {
 
 class TodoFilter  extends BaseViewComponent {
   data = ['all', 'pending', 'completed', 'removed'];
+  output = new Emitter()
+  constructor(option) {
+    super(option);
+
+    this.data.forEach(item => {
+      let button = document.createElement('button');
+      button.innerText = item;
+      this.rootEle.appendChild(button);
+      // button.addEventListener('click', () => {
+      //   console.log(item);
+      //   // this.output.emit('clickfilter', item);
+      // });
+
+      button.onclick = (event) => {
+        this.output.emit('onclickfilter', item);
+      }
+    })
+  }
+
+}
+
+class TodoItem extends BaseViewComponent {
+  data = null;
+  constructor(data) {
+    super();
+    this.data = data;
+    this.rootEle = document.createElement('li');
+    this.rootEle.innerText = this.data.title;
+  }
 }
 
 class TodoList extends BaseViewComponent { 
-  
+  output = new Emitter();
+  constructor(option) {
+    super(option);
+    this.TodoService = new TodoService();
+    this.refresh();
+  }
+
+  refresh() {
+    this.rootEle.innerHTML = '';
+    this.TodoService.todos.forEach(todo => {
+      let todoItem = new TodoItem(todo);
+      this.rootEle.appendChild(todoItem.rootEle);
+    });
+  }
 }
 
 class TodoService{
